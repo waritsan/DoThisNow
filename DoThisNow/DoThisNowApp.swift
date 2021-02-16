560e7e2c54c1926b5db2086df8b72cf9493b99dd
Warit Santaputra <waritsan@gmail.com>
2021-02-16 16:18:20 +0700

    Initial Commit

---
+++ DoThisNow/DoThisNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoThisNowApp.swift
+//  DoThisNow
+//
+//  Created by Warit Santaputra on 16/2/21.
+//
+
+import SwiftUI
+
+@main
+struct DoThisNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
